688b2765b1c42a28ceb965327f611a1ea41150c3
Alexandre <jmalex321@gmail>
2026-05-01 15:44:19 -0300

    concerto

--- terraform/network.tf
+++ terraform/network.tf
@@ -14,6 +14,7 @@ resource "aws_s3_bucket" "s3-buket-s3" {
 resource "aws_subnet" "my_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Main"
@@ -30,3 +31,22 @@ resource "aws_internet_gateway" "doom_net" {
 }
 
 
+# 1. Criar a Tabela de Rotas dizendo: "Tudo que for pra fora (0.0.0.0/0), vai pelo doom_net"
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.doom_net.id
+  }
+
+  tags = {
+    Name = "rota-para-internet"
+  }
+}
+
+# 2. Associar (Ligar) essa tabela à sua Subnet
+resource "aws_route_table_association" "my_assoc" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
--- terraform/sergure.tf
+++ terraform/sergure.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "allow_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.allow_sg.id
-  cidr_ipv4         = "177.55.205.139/32"
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
